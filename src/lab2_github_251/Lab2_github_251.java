66618e789080b9564b8e38225d282da5f54ab4b2
Layan <layab66@gmail.com>
2024-01-31 11:30:03 +0300

    simple print statement

---
+++ src/lab2_github_251/Lab2_github_251.java
@@ -0,0 +1,11 @@
+
+package lab2_github_251;
+
+public class Lab2_github_251 {
+    public static void main(String[] args) {
+     System.out.println("Hello github community"); 
+     
+        
+        
+    }
+}
